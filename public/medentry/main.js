ac84c06a5bbb59c2a32283124eeb275ea6c69c02
Joe Portner <jportner@mitre.org>
2016-03-18 16:56:59 -0400

    Fixed to ensure 'not_found' field gets submitted for each MedEntry
    HTML5 functionality dictates that an unchecked checkbox input does
    not get submitted as 'false', rather it gets omitted from the form
    submission completely. So we had to add a hidden input before it
    that acts as a default if the checkbox is not checked.

--- public/medentry/main.js
+++ public/medentry/main.js
@@ -180,7 +180,7 @@ var MedEntryInfoList = React.createClass({
                         </thead>
                     </table>
                     <table className='table table-striped table-hover'>
-                    <tbody>                  
+                    <tbody>
                         {this.state.allMedications.map(function (medication) {
                             return <MedEntryInfo fhirMedications={medication.text}
                                                  key={medication.id}
@@ -189,7 +189,7 @@ var MedEntryInfoList = React.createClass({
                             />; // display each medication
                         })}
                     </tbody>
-                    </table>                                       
+                    </table>
                 </div>
             </div>
             <div className='panel panel-default'>
@@ -204,7 +204,7 @@ var MedEntryInfoList = React.createClass({
                             hidden={this.state.submitHidden}>submit changes
                     </button>
                 </div>
-            </div>               
+            </div>
             <div hidden className='success-message' name='submit_success'>Submitted Successfully!</div>
             </form>
         );
@@ -317,24 +317,21 @@ var MedEntryInfo = React.createClass({
         //debugger;
         $( this.refs.toggleInput).bootstrapToggle();
         var myMedEntryInfo = this;
-        $( this.refs.toggleInput).on("change", function(evt) { 
+        $( this.refs.toggleInput).on("change", function(evt) {
             //console.log(this, " fired onChange");
             myMedEntryInfo.handleChange(evt);
         });
 
         $( this.refs.complianceInput).bootstrapToggle();
         var myMedEntryInfo1 = this;
-        $( this.refs.complianceInput).on("change", function(evt) { 
+        $( this.refs.complianceInput).on("change", function(evt) {
             //console.log(this, " fired onChange");
             myMedEntryInfo1.handleChange(evt);
         });
 
-        $( this.refs.prescriberInput).bootstrapToggle({
-            on: 'VA',
-            off: 'other'
-        });
+        $( this.refs.prescriberInput).bootstrapToggle();
         var myMedEntryInfo2 = this;
-        $( this.refs.prescriberInput).on("change", function(evt) { 
+        $( this.refs.prescriberInput).on("change", function(evt) {
             //console.log(this, " fired onChange");
             myMedEntryInfo2.handleChange(evt);
         });
@@ -351,7 +348,7 @@ var MedEntryInfo = React.createClass({
                 {label: 'AC', value: 'with meals'}
             ];
         return (
-            <tr>  
+            <tr>
             <td className='col-xs-2'>
                 <span className='original-med-name'>{this.state.med_name}</span>
                 <div>
@@ -365,8 +362,11 @@ var MedEntryInfo = React.createClass({
                 </div>
             </td>
             <td className='col-xs-1'>
-                <input ref='toggleInput' className='col-xs-12' type='checkbox'defaultChecked data-toggle='toggle' data-on='found' data-off='missing' name='not_found' value={this.state.not_found}
-                       hidden={!this.state.is_fhir_med} onChange={this.handleChange}/>
+                {/* we need a hidden input because an "unchecked" checkbox will not get submitted with the form...
+                        the 2nd input (if checked) will override the 1st */}
+                <input type='hidden' value='true' name='not_found' />
+                <input ref='toggleInput' className='col-xs-12' type='checkbox' defaultChecked data-toggle='toggle' data-on='found' data-off='missing' name='not_found' value={this.state.not_found}
+                       hidden={!this.state.is_fhir_med} onChange={this.handleChange} />
             </td>
             <td className='col-xs-2'>
             <div hidden={this.state.not_found}>
@@ -445,14 +445,14 @@ var MedEntryInfo = React.createClass({
             <td className='col-xs-2'>
             <div hidden={this.state.not_found}>
                 <input ref='complianceInput' className='col-xs-12' type='checkbox' defaultChecked data-toggle='toggle' data-on='yes' data-off='no' name='compliance_bool' value={this.state.compliance_bool}
-                       onClick={this.handleChange}/>
+                       onClick={this.handleChange} disabled={this.state.not_found}/>
                 <textarea className='col-xs-12 focusWhenVisible' type='text' value={this.state.compliance_note} name='noncompliance_note'
                     rows="1" onChange={this.handleChange} placeholder='please expain' hidden={this.state.compliance_bool} />
             </div>
             </td>
             <td className='col-xs-2' hidden={this.state.not_found}>
             <div hidden={this.state.not_found}>
-                <input ref='prescriberInput' className='col-xs-12' type='checkbox' type='checkbox' defaultChecked name='med_bool' value={this.state.med_bool}
+                <input ref='prescriberInput' className='col-xs-12' type='checkbox' defaultChecked data-toggle='toggle' data-on='VA' data-off='other' name='med_bool' value={this.state.med_bool}
                        onClick={this.handleChange} disabled={this.state.not_found}/>
                 <textarea className='col-xs-12 focusWhenVisible' type='text' value={this.state.prescriber_note} name='prescriber_note'
                     rows="1" onChange={this.handleChange} placeholder='please enter prescriber' hidden={this.state.med_bool}/>
@@ -463,11 +463,11 @@ var MedEntryInfo = React.createClass({
                 <textarea className='col-xs-12' type='text' name='note' value={this.state.note}
                           rows="1" onChange={this.handleChange} disabled={this.state.not_found}
                           style = {{background: 'inherit'}} />
-            </div>              
+            </div>
             </td>
             <input type='hidden' value={this.state.med_id} name='medication_id'/>
             <input type='hidden' value={this.state.order_id} name='medication_order_id'/>
-            </tr>                        
+            </tr>
         );
     }
 });
