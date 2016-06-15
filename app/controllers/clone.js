c859549ede85344113853a88df8b62e2ed418021
Joe Portner <jportner@mitre.org>
2016-06-15 17:43:42 -0400

    Added Comm (annotation) cloning
    Refactored & abstracted MedEntry cloning to reduce clutter

--- app/controllers/clone.js
+++ app/controllers/clone.js
@@ -7,6 +7,7 @@ var HTTP = require('q-io/http');
 // import internal modules
 var app = require('../../lib/app');
 var MedEntry = app.MedEntry;
+var Comm = app.Comm;
 var respond = require('../../lib/utils').respond;
 var dasherize = require('../../lib/utils').dasherize;
 
@@ -18,7 +19,7 @@ exports.clonePatient = function (req, res) {
 
     cloneFhirData(args)
         .then(cloneMedEntryData)
-        .then(cloneAnnotationData)
+        .then(cloneCommData)
         .then(cloneNominationData)
         .then(function (result) {
             // all the cloning was successful, let's return the new patient ID
@@ -40,62 +41,24 @@ function cloneFhirData(params) {
     var patientId = params.patientId;
 
     // TODO: remove this stub code
-    params.idMap = {};
+    params.idMap = {
+        bar: 'bar2',
+        baz: 'baz2',
+        herp: 'herp2',
+        derp: 'derp2'
+    };
     params.idMap[patientId] = new Date().getTime() + '-' + patientId;
     return Q.resolve(params);
 }
 
 // params: {patientId, idMap}
 function cloneMedEntryData(params) {
-    var patientId = params.patientId;
-    var idMap = params.idMap;
-
-    // recursive method to sequentially save new medentry documents
-    function saveMedEntries(medEntries) {
-        if (medEntries.length === 0) {
-            return Q.resolve();
-        }
-        var medEntry = medEntries.pop();
-        var medId = medEntry.medication_id;
-        var orderId = medEntry.medication_order_id;
-        var idNotFound;
-        if (medId.length > 0 || orderId > 0) {
-            // medId and orderId should both only be non-empty at the same time, but we check both to be safe
-            medId = idMap[medId];
-            orderId = idMap[orderId];
-            idNotFound = !medId || !orderId;
-        }
-
-        if (!idNotFound) {
-            medEntry._id = app.mongoose.Types.ObjectId();
-            medEntry.isNew = true;
-            medEntry.patient_id = idMap[patientId];
-            medEntry.medication_id = medId;
-            medEntry.medication_order_id = orderId;
-            return medEntry.saveQ()
-                .catch(function (err) {
-                    // ran into a problem cloning this particular MedEntry; skip it and continue
-                    app.logger.error('clone controller: Unable to clone MedEntry "%s" for: patientId "%s", medicationId "%s", medicationOrderId "%s":',
-                        medEntry._id, patientId, medEntry.medication_id, medEntry.medication_order_id, err.message);
-                }).thenResolve(medEntries)
-                .then(saveMedEntries);
-        } else {
-            // ran into a problem cloning this particular MedEntry; skip it and continue
-            app.logger.error('clone controller: Unable to clone MedEntry "%s" for: patientId "%s", medicationId "%s", medicationOrderId "%s" (no mapped medication ID or medication order ID)',
-                medEntry._id, patientId, medEntry.medication_id, medEntry.medication_order_id);
-            return saveMedEntries(medEntries);
-        }
-    }
-
-    return MedEntry.findQ({patient_id: patientId})
-        .then(saveMedEntries)
-        .thenResolve(params);
+    return cloneMongoDocs('MedEntry', params, ['medication_id', 'medication_order_id']);
 }
 
 // params: {patientId, idMap}
-function cloneAnnotationData(params) {
-    // TODO: remove this stub code
-    return Q.resolve(params);
+function cloneCommData(params) {
+    return cloneMongoDocs('Comm', params, ['careplan_id','resource_id']);
 }
 
 // params: {patientId, idMap}
@@ -179,4 +142,54 @@ function cloneNominationData(params) {
 
 function cloneObj(obj) {
     return JSON.parse(JSON.stringify(obj));
+}
+
+function cloneMongoDocs(model, params, attrs) {
+    var patientId = params.patientId;
+    var idMap = params.idMap;
+
+    // recursive method to sequentially save new medentry documents
+    function saveDocuments(documents) {
+        if (documents.length === 0) {
+            return;
+        }
+        var document = documents.pop();
+        var attrMap = {}; // map of attributes that should be transformed with the idMap
+        var idNotFound = false;
+        for (var i = 0; i < attrs.length; i++) {
+            var attr = attrs[i];
+            var entry = document[attr];
+            attrMap[attr] = entry;
+            if (!entry) { // should not be undefined, null, or blank
+                idNotFound = true;
+                break;
+            }
+        }
+
+        if (!idNotFound) {
+            document._id = app.mongoose.Types.ObjectId();
+            document.isNew = true;
+            document.patient_id = idMap[patientId];
+            for (var i = 0; i < attrs.length; i++) {
+                var attr = attrs[i];
+                document[attr] = idMap[document[attr]];
+            }
+            return document.saveQ()
+                .catch(function (err) {
+                    // ran into a problem cloning this particular document; skip it and continue
+                    app.logger.error('clone controller: Unable to clone %s "%s" for patientId "%s":',
+                        model, document._id, patientId, err.message);
+                }).thenResolve(documents)
+                .then(saveDocuments);
+        } else {
+            // ran into a problem cloning this particular document; skip it and continue
+            app.logger.error('clone controller: Unable to clone %s "%s" for patientId "%s" (mapped attribute ID missing)',
+                model, document._id, patientId);
+            return saveDocuments(documents);
+        }
+    }
+
+    return app[model].findQ({patient_id: patientId})
+        .then(saveDocuments)
+        .thenResolve(params);
 }
